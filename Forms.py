2dff4e4f1474dcde1467aefda4761ab530201b46
Kobot7 <estherkojialing@gmail.com>
2020-06-21 01:05:14 +0800

    Updates
    Created template for individual posts, comments, subcomments. Yet to add in upvote/downvote options.
    Added Login & Signup pages.
    Added trending topics column for home page.

--- Forms.py
+++ Forms.py
@@ -1,7 +1,20 @@
-from wtforms import Form, validators, StringField, TextAreaField
+from wtforms import Form, validators, StringField, TextAreaField, PasswordField
 
 class FeedbackForm(Form):
     firstName = StringField("First Name", [validators.DataRequired()], render_kw={"placeholder": "Alice"})
     lastName = StringField("Last Name", [validators.DataRequired()], render_kw={"placeholder": "Tan"})
     email = StringField('Email Address', [validators.Length(min=6, max=35)], render_kw={"placeholder": "alicetan@email.com"})
     comment = TextAreaField('Comment', [validators.DataRequired()], render_kw={"rows": 5, "placeholder": "Enter comment here..."})
+
+class LoginForm(Form):
+    username = StringField("Username", [validators.DataRequired()])
+    password = PasswordField('Password', [validators.DataRequired()])
+
+class SignUpForm(Form):
+    email = StringField('Email Address', [validators.Length(min=6, max=35)], render_kw={"placeholder": "alicetan@email.com"})
+    username = StringField("Username", [validators.DataRequired()])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirmPassword', message='Passwords do not match.')
+    ])
+    confirmPassword = PasswordField('Re-enter Password', [validators.DataRequired()])
--- main.py
+++ main.py
@@ -9,25 +9,46 @@ db = mysql.connector.connect(
     database="blogdb"
 )
 
-mycursor =db.cursor()
+mycursor = db.cursor()
 mycursor.execute("SHOW TABLES")
 print(mycursor)
 
 app = Flask(__name__)
 
+tempPosts = {
 
-@app.route("/", methods=["GET", "POST"])
+}
+
+@app.route('/')
 def main():
     return redirect("/home")
 
-@app.route("/home", methods=["GET", "POST"])
+@app.route('/home', methods=["GET"])
 def home():
-    return render_template("home.html", currentPage="home")
+    return render_template('home.html', currentPage='home')
 
-@app.route("/feedback")
+@app.route('/viewPost')
+def viewPost():
+    return render_template('viewPost.html', currentPage='viewPost')
+
+@app.route('/addPost', methods=["GET", "POST"])
+def addPost():
+    return render_template('addPost.html', currentPage='home')
+
+@app.route('/feedback')
 def feedback():
     feedbackForm = Forms.FeedbackForm(request.form)
-    return render_template("feedback.html", currentPage="feedback",feedbackForm = feedbackForm)
+    return render_template('feedback.html', currentPage='feedback', feedbackForm = feedbackForm)
+
+@app.route('/login')
+def login():
+    loginForm = Forms.LoginForm(request.form)
+    return render_template('login.html', currentPage='login', loginForm = loginForm)
+
+@app.route('/signup')
+def signUp():
+    signUpForm = Forms.SignUpForm(request.form)
+    return render_template('signup.html', currentPage='signUp', signUpForm = signUpForm)
 
 if __name__ == "__main__":
     app.run(debug=True)
