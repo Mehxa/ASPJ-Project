4ccdebdd984a6b4b66b37a435c8fa7db430d3912
Kobot7 <estherkojialing@gmail.com>
2020-06-21 23:17:21 +0800

    Update
    Sign Up function works with db.
    Added buttons for voting and reply to posts/comment.
    Partial code for feedback. Unable to add feedback to db as login function hasn't been programmed.

--- Forms.py
+++ Forms.py
@@ -4,6 +4,7 @@ class FeedbackForm(Form):
     firstName = StringField("First Name", [validators.DataRequired()], render_kw={"placeholder": "Alice"})
     lastName = StringField("Last Name", [validators.DataRequired()], render_kw={"placeholder": "Tan"})
     email = StringField('Email Address', [validators.Length(min=6, max=35)], render_kw={"placeholder": "alicetan@email.com"})
+    reason = StringField("Reason", [validators.DataRequired()], render_kw={"placeholder": "Feedback regarding post moderation"})
     comment = TextAreaField('Comment', [validators.DataRequired()], render_kw={"rows": 5, "placeholder": "Enter comment here..."})
 
 class LoginForm(Form):
@@ -11,8 +12,9 @@ class LoginForm(Form):
     password = PasswordField('Password', [validators.DataRequired()])
 
 class SignUpForm(Form):
-    email = StringField('Email Address', [validators.Length(min=6, max=35)], render_kw={"placeholder": "alicetan@email.com"})
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
     username = StringField("Username", [validators.DataRequired()])
+    name = StringField("Full Name", [validators.DataRequired()])
     password = PasswordField('New Password', [
         validators.DataRequired(),
         validators.EqualTo('confirmPassword', message='Passwords do not match.')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector
+import mysql.connector, re
 import Forms
+from datetime import datetime
 
 db = mysql.connector.connect(
     host="localhost",
@@ -9,7 +10,7 @@ db = mysql.connector.connect(
     database="blogdb"
 )
 
-mycursor = db.cursor()
+mycursor = db.cursor(buffered=True)
 mycursor.execute("SHOW TABLES")
 print(mycursor)
 
@@ -35,19 +36,54 @@ def viewPost():
 def addPost():
     return render_template('addPost.html', currentPage='home')
 
-@app.route('/feedback')
+@app.route('/feedback', methods=["GET", "POST"])
 def feedback():
     feedbackForm = Forms.FeedbackForm(request.form)
+
+    if request.method == 'POST' and feedbackForm.validate():
+        mycursor.execute('SELECT FeedbackID FROM feedback WHERE FeedbackID=(SELECT max(FeedbackID) FROM feedback)')
+        lastIdRegistered = mycursor.fetchall()
+        availId = lastIdRegistered[0][0] + 1
+        dateTime = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+        sql = 'INSERT INTO feedback (FeedbackID, UserID, Reason, Content, DateTimePosted) VALUES (%s, %s, %s, %s, %s)'
+        # Need to link to current userID to work
+        val = (availId, 1, feedbackForm.reason.data, feedbackForm.comment.data, dateTime)
+        mycursor.execute(sql, val)
+        db.commit()
+
     return render_template('feedback.html', currentPage='feedback', feedbackForm = feedbackForm)
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
     loginForm = Forms.LoginForm(request.form)
+
     return render_template('login.html', currentPage='login', loginForm = loginForm)
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signUp():
     signUpForm = Forms.SignUpForm(request.form)
+
+    if request.method == 'POST' and signUpForm.validate():
+        if not(re.search('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', signUpForm.email.data)):
+            signUpForm.email.errors.append('Invalid email address.')
+            pass
+
+        mycursor.execute('SELECT UserID FROM user WHERE UserID=(SELECT max(UserID) FROM user)')
+        lastIdRegistered = mycursor.fetchall()
+        availId = lastIdRegistered[0][0] + 1
+        sql = 'INSERT INTO user (UserID, Name, Email, Username, Password, isAdmin) VALUES (%s, %s, %s, %s, %s, %s)'
+        val = (availId, signUpForm.name.data, signUpForm.email.data, signUpForm.username.data, signUpForm.password.data, 0)
+        try:
+            mycursor.execute(sql, val)
+            db.commit()
+
+        except mysql.connector.errors.IntegrityError as errorMsg:
+            errorMsg = str(errorMsg)
+            if 'email' in errorMsg.lower():
+                signUpForm.email.errors.append('The email has already been linked to another account. Please use a different email.')
+            elif 'username' in errorMsg.lower():
+                signUpForm.username.errors.append('This username is already taken.')
+
     return render_template('signup.html', currentPage='signUp', signUpForm = signUpForm)
 
 if __name__ == "__main__":
